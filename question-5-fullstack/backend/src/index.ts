2856b06e272317e457d5aeb44fe7ed23a01f1fbe
aphisit.ths <aphisit.thu@gmail.com>
2025-03-22 17:39:00 +0700

    (q5)[backend] - allow cors and add remove .env from ignore (dont have secret)

--- question-5-fullstack/backend/src/index.ts
+++ question-5-fullstack/backend/src/index.ts
@@ -1,35 +1,37 @@
-import {
-    Elysia,
-    t,
-} from "elysia";
-import 'dotenv/config';
-import {healthChecks} from "./db/schema";
-import {db} from "./db/db";
-import {UserController} from "./modules/users/user.controller";
-import {CustomError, handleErrorResponse} from "./middleware/exceptions";
+import { Elysia, t } from "elysia";
+import "dotenv/config";
+import { healthChecks } from "./db/schema";
+import { db } from "./db/db";
+import { UserController } from "./modules/users/user.controller";
+import { CustomError, handleErrorResponse } from "./middleware/exceptions";
+import { cors } from "@elysiajs/cors";
 
-
-
-const DEFAULT_PORT: number = 7777;
-
+const DEFAULT_PORT: number = 2222;
 
 const getHealthStatus = async (): Promise<unknown> => {
     try {
         return await db.select().from(healthChecks).limit(1);
     } catch (error) {
-        throw new Error('Database health check failed');
+        throw new Error("Database health check failed");
     }
 };
 
 // Main application setup
 const app = new Elysia()
+    .use(
+        cors({
+            origin: ["*"],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            credentials: true,
+        })
+    )
     .error(CustomError)
-    .onError(({code, error, set}) => {
+    .onError(({ code, error, set }) => {
         return handleErrorResponse(set, error, code);
     })
     .use(UserController)
-    .get('/', async () => await getHealthStatus(), {
-        response: t.Any()
+    .get("/", async () => await getHealthStatus(), {
+        response: t.Any(),
     });
 
 const startServer = async (): Promise<void> => {
@@ -38,7 +40,7 @@ const startServer = async (): Promise<void> => {
         await app.listen(port);
         console.log(`🦊 Elysia is running at ${app.server?.hostname}:${port}`);
     } catch (error) {
-        console.error('Failed to start server:', error);
+        console.error("Failed to start server:", error);
         process.exit(1);
     }
 };
